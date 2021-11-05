44e19cfc236cb259be8fd9b94008f6326721cde8
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-11-05 12:16:47 +0100

    First packaged version

---
+++ Development/Create_the_MarkdownHelpers_Package.R
@@ -0,0 +1,124 @@
+######################################################################################################
+# Create_the_MarkdownHelpers_Package.v0.1.R
+# 31 10 2021
+######################################################################################################
+# source("/Users/abel.vertesy/GitHub/Packages/MarkdownHelpers/Development/Create_the_MarkdownHelpers_Package.v0.1.R")
+rm(list = ls(all.names = TRUE));
+try(dev.off(), silent = TRUE)
+
+
+# Functions ------------------------
+# install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org") # install.packages("devtools")
+require("devtools")
+require("roxygen2")
+require("stringr")
+
+# devtools::install_github(repo = "vertesy/CodeAndRoll2")
+require('CodeAndRoll2')
+require('Stringendo')
+
+
+# Setup ------------------------
+PackageName = 	"MarkdownHelpers"
+setwd("~/GitHub/Packages/")
+
+RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
+fname = 	kollapse(PackageName, ".R")
+Package_FnP = 	kollapse(RepositoryDir, "R/", fname)
+
+BackupDir = "~/GitHub/Packages/MarkdownHelpers/Development/"
+dir.create(BackupDir)
+
+# devtools::use_package("vioplot")
+DESCRIPTION <- list("Title" = "MarkdownHelpers"
+    , "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
+    , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
+    , "Description" = "MarkdownHelpers is a set of R functions to parse Markdown and other generic helpers."
+    , "License" = "GPL-3 + file LICENSE"
+    , "Version" = "0.1.1"
+    , "Packaged" =  Sys.time()
+    , "Repository" =  "CRAN"
+    , "Depends" =  "Stringendo"
+    , "Imports" = "base, devtools, grDevices, usethis, utils"
+    # , "Suggests" = ""
+    , "BugReports"= "https://github.com/vertesy/MarkdownHelpers/issues"
+)
+
+
+setwd(RepositoryDir)
+if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = DESCRIPTION, rstudio = TRUE)
+} else {
+    getwd()
+    try(file.remove(c("DESCRIPTION","NAMESPACE", "MarkdownHelpers.Rproj")))
+    create_package(path = RepositoryDir, fields = DESCRIPTION, open = F)
+}
+
+
+# go and write fun's ------------------------------------------------------------------------
+# file.edit(Package_FnP)
+
+# Create Roxygen Skeletons ------------------------
+# RoxygenReady(Package_FnP)
+
+# replace output files ------------------------------------------------
+BackupOldFile = 	kollapse(BackupDir, "Development", ".bac", print = FALSE)
+AnnotatedFile = 	kollapse(BackupDir, "Development", ".annot.R", print = FALSE)
+file.copy(from = Package_FnP, to = BackupOldFile, overwrite = TRUE)
+# file.copy(from = AnnotatedFile, to = Package_FnP, overwrite = TRUE)
+
+# Manual editing of descriptors ------------------------------------------------
+# file.edit(Package_FnP)
+
+# Compile a package ------------------------------------------------
+setwd(RepositoryDir)
+getwd()
+document()
+
+
+# Install your package ------------------------------------------------
+# # setwd(RepositoryDir)
+install(RepositoryDir, upgrade = F)
+
+# require("MarkdownHelpers")
+# # remove.packages("MarkdownHelpers")
+# # Test your package ------------------------------------------------
+# help("wplot")
+# cat("\014")
+# devtools::run_examples()
+
+
+# Test if you can install from github ------------------------------------------------
+# devtools::install_github(repo = "vertesy/MarkdownHelpers")
+
+# require("MarkdownHelpers")
+
+# Clean up if not needed anymore ------------------------------------------------
+# View(installed.packages())
+# remove.packages("MarkdownHelpers")
+
+check(RepositoryDir, cran = TRUE)
+# as.package(RepositoryDir)
+#
+#
+# # source("https://install-github.me/r-lib/desc")
+# # library(desc)
+# # desc$set("MarkdownHelpers", "foo")
+# # desc$get(MarkdownHelpers)
+#
+#
+# system("cd ~/GitHub/MarkdownHelpers/; ls -a; open .Rbuildignore")
+
+# Check package dependencies ------------------------------------------------
+depFile = paste0(RepositoryDir, 'Development/Dependencies.R')
+
+(f.deps <- NCmisc::list.functions.in.file(filename = Package_FnP))
+# clipr::write_clip(f.deps)
+
+sink(file = depFile); print(f.deps); sink()
+p.deps <- gsub(x = names(f.deps), pattern = 'package:', replacement = '')
+write(x = p.deps, file = depFile, append = T)
+p.dep.declared <- trimws(unlist(strsplit(DESCRIPTION$Imports, ",")))
+p.dep.new <- sort(union( p.deps, p.dep.declared))
+# clipr::write_clip(p.dep.new)
+
+
