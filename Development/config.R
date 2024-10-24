201a5744039102798b90e76c4e84f9f1cd79ebd2
vertesy <abel.vertesy@imba.oeaw.ac.at>
2024-10-24 16:21:40 +0200

    ...

--- Development/config.R
+++ Development/config.R
@@ -1,5 +1,5 @@
 # Configuration for the Package
-# file.edit("~/GitHub/Packages/XXXXXXXXXX/Development/config.R")
+# file.edit("~/GitHub/Packages/MarkdownHelpers/Development/config.R")
 
 DESCRIPTION <- list(
   package.name = "MarkdownHelpers",
