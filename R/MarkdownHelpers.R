bf6fe181ae1812c693ad706aca12dff6174ca103
Abel Vertesy <vertesy@users.noreply.github.com>
2025-08-06 12:56:55 +0200

    Fix spelling and grammar in comments (#7)

--- R/MarkdownHelpers.R
+++ R/MarkdownHelpers.R
@@ -44,7 +44,7 @@ irequire <- function(package) {
     print("Loading package:")
     require(package = package_, character.only = TRUE)
   }
-} # install package if cannot be loaded
+} # install package if it cannot be loaded
 
 
 # ______________________________________________________________________________________________________________________________
@@ -170,7 +170,7 @@ lookup <- function(needle, haystack, exact = TRUE, report = FALSE) { # Awesome p
 #'
 #' @export
 
-combine.matrices.by.rowname.intersect <- function(matrix1, matrix2, k = 2) { # combine matrices by rownames intersect
+combine.matrices.by.rowname.intersect <- function(matrix1, matrix2, k = 2) { # combine matrices by row name intersection
   rn1 <- rownames(matrix1)
   rn2 <- rownames(matrix2)
   idx <- intersect(rn1, rn2)
@@ -341,7 +341,7 @@ md.image.linker <- function(fname_wo_ext, OutDir_ = ww.set.OutDir()) {
     } else {
       if (exists("b.Subdirname") && !b.Subdirname == FALSE) {
         fname_wo_ext <- paste0(b.Subdirname, "/", fname_wo_ext)
-      } # set only if b.Subdirname is defined, it is not FALSE.
+      } # set only if b.Subdirname is defined and not FALSE.
       llogit(kollapse("![", fn, "]", "(", fname_wo_ext, ".png)", print = FALSE))
     }
   } else {
@@ -1196,7 +1196,7 @@ ww.autoPlotName <- function(name = NULL) {
 
 ww.assign_to_global <- function(name, value, pos = 1, max_print = 5, verbose = TRUE) {
   if (verbose) {
-    Stringendo::iprint(name, "defined as:") # , "is a new global environment variable"
+    Stringendo::iprint(name, "defined as:") # "is a new global environment variable"
     print(utils::head(value, max_print))
   }
   assign(name, value, envir = as.environment(pos))
@@ -1319,7 +1319,7 @@ wcolorize <- function(vector = c(1, 1, 1:6),
   COLZ <- CodeAndRoll2::as.numeric.wNames.factor(vector) # if basic numbers
   if (randomize) {
     COLZ <- sample(COLZ)
-  } # if randomise
+  } # if randomize
   if (RColorBrewerSet != FALSE) {
     COLZ <- RColorBrewer::brewer.pal(NrCol, name = RColorBrewerSet)[CodeAndRoll2::as.numeric.wNames.factor(vector)]
   } else {
