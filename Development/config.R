ebe068cf1ac5ff38d4f8ffa8b5ac5c74e583d55b
Abel Vertesy <vertesy@users.noreply.github.com>
2026-09-02 15:51:14 +0200

    intermediate stage

--- Development/config.R
+++ Development/config.R
@@ -3,7 +3,7 @@
 
 DESCRIPTION <- list(
   package.name = "MarkdownHelpers",
-  version = "1.3.0",
+  version = "1.3.2",
   title = "MarkdownHelpers",
   description = "MarkdownHelpers is a set of R functions to parse and Markdown files and other generic helpers.",
 
